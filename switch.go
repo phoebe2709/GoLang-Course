6bf8f6dc1e1c1f459a511f81d2610550ac22a4ac
Phoebe Rowland <61206950+phoebe2709@users.noreply.github.com>
2020-09-21 18:56:05 +0100

    Add files via upload

---
+++ switch.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+func main() {
+	switch {
+	case false:
+		fmt.Println("this should not print")
+	case (2 == 4):
+		fmt.Println("this should not print2")
+	case (3 == 3):
+		fmt.Println("prints")
+		fallthrough
+	case (4 == 4):
+		fmt.Println("also true, does it print")
+	default:
+		fmt.Println("this is default")
+	}
+}
