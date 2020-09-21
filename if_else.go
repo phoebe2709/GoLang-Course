35d40874c36faf929788c6c200ee90b7868fef13
Phoebe Rowland <61206950+phoebe2709@users.noreply.github.com>
2020-09-21 18:41:03 +0100

    Add files via upload

---
+++ if_else.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+func main() {
+	x := 27
+	if x == 27 {
+		fmt.Println("Our value is 27")
+	} else if x == 28 {
+		fmt.Println(" our value is 28")
+	} else {
+		fmt.Println("our value was not 27")
+	}
+}
