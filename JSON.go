2a9d8c784a3f54b29b85b97a2c4873c6c633fce9
Phoebe Rowland <61206950+phoebe2709@users.noreply.github.com>
2020-11-27 15:04:03 +0000

    Add files via upload

---
+++ JSON.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func main() {
+	p1 := person{
+		First: "Phoebe",
+		Last:  "Rowland",
+		Age:   32,
+	}
+
+	p2 := person{
+		First: "Sophie",
+		Last:  "Rowland",
+		Age:   30,
+	}
+
+	people := []person{p1, p2}
+
+	fmt.Println(people)
+
+	bs, err := json.Marshal(people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs))
+}
